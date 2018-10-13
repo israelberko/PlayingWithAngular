d123a04784169c440f95859c44c6aaf8a0ce7992
israelb <israel.berko@cellwize.com>
2018-10-13 19:50:07 +0300

    adding entities

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,18 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { PlayingWithAngularRfbLocationModule } from './rfb-location/rfb-location.module';
+import { PlayingWithAngularRfbEventModule } from './rfb-event/rfb-event.module';
+import { PlayingWithAngularRfbEventAttendanceModule } from './rfb-event-attendance/rfb-event-attendance.module';
+import { PlayingWithAngularRfbUserModule } from './rfb-user/rfb-user.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        PlayingWithAngularRfbLocationModule,
+        PlayingWithAngularRfbEventModule,
+        PlayingWithAngularRfbEventAttendanceModule,
+        PlayingWithAngularRfbUserModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
